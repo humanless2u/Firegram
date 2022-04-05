03af965eaa41eb26f07e258e14aeb682138864ac
humanless2u <humanless2u@protonmail.com>
2022-04-05 10:40:24 -0300

    fix

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import { projectFirestore } from "../firebase/config";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot, orderBy } from "firebase/firestore";
 
 const useFirestore = (collectionName) => {
   const [docs, setDocs] = useState([]);
 
   useEffect(() => {
     const collectionRef = collection(projectFirestore, collectionName);
-
+    orderBy("createdAt", "asc");
     onSnapshot(collectionRef, (snap) => {
       let documents = [];
 
